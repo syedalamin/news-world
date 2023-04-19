85edab3a1a77679a7fec1190b412c63331e2d8a6
SyedAlamin <syedalamin2580@gmail.com>
2023-04-20 01:37:24 +0600

    right nav

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../../assets/logo.png';
 import moment from 'moment';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -19,6 +19,24 @@ const Header = () => {
                     ..... I can be a React component, multiple React components, or just some text.
                 </Marquee>
             </div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                <Button variant="dark">Dark</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </Container>
     );
 };
