52f8818c8d7ef0bfd8307c47d9b79946429cd1f5
SyedAlamin <syedalamin2580@gmail.com>
2023-04-21 19:24:30 +0600

    news

--- src/layouts/NewsLayout.jsx
+++ src/layouts/NewsLayout.jsx
@@ -1,21 +1,16 @@
-import React from 'react';
-import Header from '../pages/Shared/Header/Header';
 import Footer from '../pages/Shared/Footer/Footer';
 import { Container , Row , Col } from 'react-bootstrap';
 import LeftNav from '../pages/Shared/LeftNav/LeftNav';
 import RightNav from '../pages/Shared/RightNav/RightNav';
 import { Outlet } from 'react-router-dom';
 
-const Main = () => {
+
+const NewsLayout = () => {
     return (
         <div>
-            <Header></Header>
             <Container>
                 <Row>
-                    <Col lg={3}>
-                        <LeftNav></LeftNav>
-                    </Col>
-                    <Col lg={6}>
+                    <Col lg={9}>
                         <Outlet></Outlet>
                     </Col>
                     <Col lg={3}>
@@ -23,9 +18,8 @@ const Main = () => {
                     </Col>
                 </Row>
             </Container>
-            <Footer></Footer>
         </div>
     );
 };
 
-export default Main;
+export default NewsLayout;
